b9bc51cd9e73dadefd17b14075373011f57f399a
omarseyal <omarseyal@gmail.com>
2014-03-09 18:19:26 -0700

    solution for problem 5 inputted.

--- problem4.rb
+++ problem4.rb
@@ -1,22 +1,35 @@
+max = 999*999
+min = 900*900
+
 beginning_time = Time.now
+solution = 0;
+base = 0
+vals = []
 
-num = 600851475143
-
-
-x = Math::sqrt(num).to_i
-
-if (x%2 == 0)
-	x -= 1
-end
-
-max = 0
+x = max
 
 begin
-  max = x if ((num % x == 0) and x.prime?)
-  x -= 2
-end while ((x > 2) && (max == 0))
+	if (x.to_s.reverse == x.to_s)
+		vals << x
+	end
+	x -= 1
+end while (x >= min)
+
+vals.each do | x |
+	if ( x%10 == 9 )
+		y = 999
+		begin
+			if ( x%y == 0 )
+				solution = x
+				base = y
+			end
+			y -= 1
+		end while (y >= 100) && ( (x/y) < 1000)
+	end
+end
+
 
 end_time = Time.now
 
 puts "Time elapsed #{(end_time - beginning_time)*1000} milliseconds"
-puts "The solution is #{max}."
+puts "The solution is #{solution}; a product of #{base} and #{solution/base}."
---
+++ problem5.rb
@@ -0,0 +1,31 @@
+require 'prime'
+
+beginning_time = Time.now
+
+start_value = 1
+end_value = 20
+
+factors = {}
+
+(1...20).each do |n|
+  new_factors = n.prime_division
+  new_factors.each do |factor_pair|
+    if !(factors.has_key? factor_pair[0])
+      factors[factor_pair[0]] = factor_pair[1]
+    else
+      factors[factor_pair[0]] = (factors[factor_pair[0]] > factor_pair[1]) ? (factors[factor_pair[0]]) : (factor_pair[1])
+    end
+  end
+end
+
+solution = 0
+
+factors.each do |k, v|
+  solution += k**v
+end
+
+end_time = Time.now
+
+puts "Time elapsed #{(end_time - beginning_time)*1000} milliseconds"
+puts "The solution is #{solution}."
+puts "The unique factors are: #{factors}"
